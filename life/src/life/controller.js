ada7692d9680f0a0fa65f4c0ecf222b0efd8a49a
MWR <mwr@epcon.pl>
2018-10-23 11:23:19 +0200

    interfejs

--- life/src/life/controller.js
+++ life/src/life/controller.js
@@ -2,20 +2,117 @@ const Life = require('./life').Life;
 
 const WHITE = "rgb(255,255,255)";
 const BLACK = "rgb(0, 0, 0)";
+const GRAY = "rgb(127,127,127)";
 
 class LifeController {
-    constructor(canvasId, life) {
-        const canvas = document.getElementById(canvasId);
+    constructor(life, canvasId) {
+        this.life = life;
+        this.pixelWidth = 15;
+        this.millis = 250;
+        this.stopped = true;
+        this.initCanvas(canvasId);
+    }
+
+    initStartBtn(id) {
+        const btn = document.getElementById(id);
+        btn.onclick = this.start.bind(this);
+        return this;
+    }
+
+    initStopBtn(id) {
+        const btn = document.getElementById(id);
+        btn.onclick = this.stop.bind(this);
+        return this;
+    }
+
+    initShowCellsBtn(id) {
+        const btn = document.getElementById(id);
+        btn.onclick = this.showCells.bind(this);
+        return this;
+    }
+
+    initSlowDownBtn(id) {
+        const btn = document.getElementById(id);
+        btn.onclick = this.slowDown.bind(this);
+        return this;
+    }
+
+    initSpeedUpBtn(id) {
+        const btn = document.getElementById(id);
+        btn.onclick = this.speedUp.bind(this);
+        return this;
+    }
+
+    slowDown() {
+        this.millis += 50;
+    }
+
+    speedUp() {
+        if (this.millis > 50) {
+            this.millis -= 50;
+        }
+    }
+
+
+    showCells() {
+        console.log('stopped = ', this.stopped);
+        console.log('[' + this.life.liveCells.map(cell => `[${cell[0]}, ${cell[1]}]`).join(',') + ']');
+    }
+
+    initCanvas(id) {
+        const canvas = document.getElementById(id);
         canvas.onclick = this.onClick.bind(this);
         this.width = canvas.width;
         this.height = canvas.height;
         this.context = canvas.getContext('2d');
-        this.life = life;
-        this.pixelWidth = 3;
+    }
+
+    init() {
+        this.paint();
+
+        _next = () => {
+            if (!this.stopped) {
+                this.paint();
+                this.life.nextGen();
+            }
+
+            setTimeout(() => _next(), this.millis);
+        }
+        _next();
+    }
+
+    start() {
+        this.stopped = false;
+    }
+
+    stop() {
+        this.stopped = true;
+    }
+
+    paintBackground() {
+        this.context.fillStyle = WHITE;
+        this.context.fillRect(0, 0, this.width, this.height);
+    }
+
+    paintGrid() {
+        this.context.strokeStyle = GRAY;
+        this.context.lineWidth = 1;
+        for (let x = 0; x < this.width; x += this.pixelWidth) {
+            this.context.beginPath();
+            this.context.moveTo(x, 0);
+            this.context.lineTo(x, this.height);
+            this.context.stroke();
+        }
+
+        for (let y = 0; y < this.height; y += this.pixelWidth) {
+            this.context.beginPath();
+            this.context.moveTo(0, y);
+            this.context.lineTo(this.width, y);
+            this.context.stroke();
+        }
     }
 
     onClick(event) {
-        console.log(event);
         const x = event.offsetX;
         const y = event.offsetY;
 
@@ -23,7 +120,6 @@ class LifeController {
         const j = Math.floor(y / this.pixelWidth);
 
         this.life.toggle([i, j]);
-        console.log(this.life.liveCells.map(cell => `[${cell[0]}, ${cell[1]}]`).join(','));
         this.paint();
     }
 
@@ -35,8 +131,8 @@ class LifeController {
     }
 
     paint() {
-        this.context.fillStyle = WHITE;
-        this.context.fillRect(0, 0, this.width, this.height);
+        this.paintBackground();
+        this.paintGrid();
 
         this.context.fillStyle = BLACK;
         this.life.liveCells.forEach(cell => {
